9de402220e4a3cc102de6732b715e94c95be1d79
Eric Lamontagne <ricklamontagne@gmail.com>
2021-11-14 19:41:43 -0500

    feat: Add the ability to the deposit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Decentralized-Bank
+
+- A decentralized bank where you can deposit and withdraw cryptocurrency, as well as earn interest in the bank's token.
+
+- The total supply of token starts at zero
+
+- (10% APY) for min. deposit (0.01 ETH)
--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -4,18 +4,26 @@ pragma solidity >=0.6.0 <0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-  //add minter variable
+    address public minter;
 
-  //add minter changed event
+    event MinterChanged(address from, address to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
-    //asign initial minter
-  }
+    constructor() public payable ERC20("Decentralized Bank Currency", "DBC") {
+        minter = msg.sender;
+    }
 
-  //Add pass minter role function
+    function passMinterRole(address dBank) public {
+        require(
+            msg.sender == minter,
+            "Error, only owner can pass the minter role"
+        );
+        minter = dBank;
 
-  function mint(address account, uint256 amount) public {
-    //check if msg.sender have minter role
-		_mint(account, amount);
-	}
+        emit MinterChanged(msg.sender, dBank);
+    }
+
+    function mint(address account, uint256 amount) public {
+        require(msg.sender == minter, "Error, only the minter can mint tokens");
+        _mint(account, amount);
+    }
 }
